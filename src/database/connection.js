a33d9e36556c13e18507232d5f1d036a371f9c06
ariyantoo58 <macbook@macbooks-MacBook-Pro.local>
2023-12-19 12:10:16 +0700

    updated

--- src/database/connection.js
+++ src/database/connection.js
@@ -1,16 +1,16 @@
 "use client";
 import Sequelize from "sequelize";
-import config from "./config/config.json";
+import config from "./config/config";
 
 let sequelize;
 if (process.env.NODE_ENV === "production") {
-  sequelize = new Sequelize(config.production);
+	sequelize = new Sequelize(config.production);
 } else if (process.env.NODE_ENV === "staging") {
-  sequelize = new Sequelize(config.staging);
+	sequelize = new Sequelize(config.staging);
 } else if (process.env.NODE_ENV === "test") {
-  sequelize = new Sequelize(config.test);
+	sequelize = new Sequelize(config.test);
 } else {
-  sequelize = new Sequelize(config.development);
+	sequelize = new Sequelize(config.development);
 }
 
 const connection = sequelize;
